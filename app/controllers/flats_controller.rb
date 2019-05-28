2c261fc03187e7d556c75088e1a2afaf893ee9f9
Julie Dacosse <julie.dacosse@gmail.com>
2019-05-28 12:26:48 +0200

    worked on all flats controller methods and modified show and index views

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,25 +1,45 @@
 class FlatsController < ApplicationController
   def index
+    @flats = Flat.all
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
 
   def show
+    @flat = Flat.find(params[:id])
   end
 
   def search
   end
 
   def edit
+    @flat = Flat.find(params[:id])
   end
 
   def update
+    @flat = Flat.find(params[:id])
+    @flat.update(params[:flat])
   end
 
   def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+  end
+
+  # private
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :price_per_night, :housing_type, :max_ppl, :latitude, :longitute)
   end
 end
