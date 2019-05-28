ab6967db7d1172bcf445cba6b0cec9c145cdda1d
Krishna Krishna <kvanst3@gmail.com>
2019-05-28 12:13:51 +0200

    seeded flat and user, added faker gem, and foreign key in Flat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_145801) do
+ActiveRecord::Schema.define(version: 2019_05_28_094722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_05_27_145801) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2019_05_27_145801) do
 
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
+  add_foreign_key "flats", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 50.times do
+#   ename = Faker::Internet.email
+#   epass = Faker::Internet.password
+#   User.create(email: ename, password: epass)
+#   puts "Done Seeding..."
+# end
+
+
+flats_attribute = [
+  {
+    title: "neat house",
+    description:"this is a place",
+    price_per_night:34,
+    housing_type:"house",
+    max_ppl:4,
+    longitude:48.8566,
+    latitude:2.3522,
+    user_id: 1
+  },
+  {
+    title: " good house",
+    description:"this is a place",
+    price_per_night:100,
+    housing_type:"house",
+    max_ppl:6,
+    longitude:50.8503,
+    latitude:4.3517,
+    user_id: 2
+  },
+    {
+    title: "neat appartment",
+    description:"this is a place",
+    price_per_night:100,
+    housing_type:"appartment",
+    max_ppl:10,
+    longitude:38.7223,
+    latitude:9.1393,
+    user_id: 3
+  },
+    {
+    title: "good appartment",
+    description:"this is a place",
+    price_per_night:100,
+    housing_type:"appartment",
+    max_ppl:2,
+    longitude:48.2082,
+    latitude:16.3738,
+    user_id: 4
+  },
+    {
+    title: "filthy house",
+    description:"this is a place",
+    price_per_night:100,
+    housing_type:"house",
+    max_ppl:4,
+    longitude:51.5074,
+    latitude:0.1278,
+    user_id: 5
+  },
+    {
+    title: " filthy appartment",
+    description:"this is a place",
+    price_per_night:55,
+    housing_type:"room",
+    max_ppl:3,
+    longitude:35.2809,
+    latitude:149.1300,
+    user_id: 6
+  }
+]
+
+Flat.create!(flats_attribute)
+puts "Done Seeding.."
