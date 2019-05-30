0fbf29d7f67914e81baefa2252d037a2c8cb1744
Krishna Krishna <kvanst3@gmail.com>
2019-05-30 14:00:26 +0200

    before breakinmg

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -88,19 +88,11 @@ end
   # <%= f.input :photo%>
   # <%= f.input :photo_cache, as: :hidden %>
 
-#index page
-# <div class="container">
-#   <%= link_to 'Add new', new_flat_path %>
-#   <div class="cards">
-#     <% @flats.each do |flat|%>
-#       <div class="card d-flex flex-column">
-#         <%= link_to '<i class="far fa-trash-alt"></i>'.html_safe , flat_path(flat), method: :delete, id: "trash" %>
-#         <div> <%= link_to flat.title, flat_path(flat)  %></div>
-#        <div> <%= flat.description %> </div>
-#        <div> <%= flat.housing_type %> </div>
-#        <div> <%= flat.max_ppl %> </div>
-#        <div> <%= flat.price_per_night %> </div>
-#       </div>
-#     <% end %>
-#   </div>
-# </div>
+  #to be placed in MyFlats - Dashboard controller
+  # <% if policy(flat).destroy? %>
+  #   <%= link_to '<i class="far fa-trash-alt"></i>'.html_safe , flat_path(flat), method: :delete, id: "trash" %>
+  # <% end %>
+
+  # <% if policy(@flat).edit? %>
+  #   <div class="row"><%= link_to "Edit", edit_flat_path(@flat) %></div>
+  # <% end %>
